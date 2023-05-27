5639290598d0c1a08c8bf1423c03c8347183ba33
genugx-x <qparanoya@gmail.com>
2023-05-27 12:24:22 +0900

    프로젝트 초기화

---
+++ src/main/java/com/sungbums/d4dm/D4dmApplication.java
@@ -0,0 +1,13 @@
+package com.sungbums.d4dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D4dmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D4dmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungbums/d4dm/D4dmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungbums.d4dm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D4dmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
